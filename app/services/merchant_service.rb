8e10d83bd32f971d6a9d2d9d287f0740edcb8b50
Alex Pitzel <pitzelalex@gmail.com>
2023-02-15 15:37:23 -0500

    feat: Create merchant service _show

--- app/services/merchant_service.rb
+++ app/services/merchant_service.rb
@@ -10,8 +10,13 @@ class MerchantService
     JSON.parse(response.body, symbolize_names: true)
   end
 
-  def self.all
+  def self.index
     response = Faraday.get('http://localhost:3000/api/v1/merchants')
+    parse(response)
+  end
+
+  def self.show(id)
+    response = Faraday.get("http://localhost:3000/api/v1/merchants/#{id}")
     parse(response)
   end
 end
