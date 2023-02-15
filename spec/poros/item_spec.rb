10ebc6c5f3a7edb64a34d833579cbf4325fe54fa
Alex Pitzel <pitzelalex@gmail.com>
2023-02-15 17:05:30 -0500

    refactor: Update item poro to store description and unit_price

--- spec/poros/item_spec.rb
+++ spec/poros/item_spec.rb
@@ -18,6 +18,8 @@ RSpec.describe Item, type: :poro do
       expect(item).to be_an Item
       expect(item.id).to eq(2425)
       expect(item.name).to eq('Item Excepturi Rem')
+      expect(item.description).to eq('Perferendis reprehenderit fugiat sit eos. Corporis ipsum ut. Natus molestiae quia rerum fugit quis. A cumque doloremque magni.')
+      expect(item.unit_price).to eq(476.82)
     end
   end
 end
