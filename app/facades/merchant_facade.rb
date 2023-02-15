30ad71ec36eb3565f6b81ba91b8ceab9bf99253a
Alex Pitzel <pitzelalex@gmail.com>
2023-02-15 15:39:39 -0500

    feat: Create merchant facade _show

--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -2,13 +2,21 @@
 
 # Merchant Facade
 class MerchantFacade
-  def self.all
-    data = MerchantService.all
+  def self.index
+    data = MerchantService.index
 
     merchants = data[:data]
 
     merchants.map do |merchant_data|
       Merchant.new(merchant_data)
     end
+  end
+
+  def self.show(id)
+    data = MerchantService.show(id)
+
+    merchant = data[:data]
+
+    Merchant.new(merchant)
   end
 end
