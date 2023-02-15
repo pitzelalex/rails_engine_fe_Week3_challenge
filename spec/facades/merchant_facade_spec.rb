db946c248862edc434d35e143f932cf1681f5522
Alex Pitzel <pitzelalex@gmail.com>
2023-02-15 15:36:44 -0500

    test: Create return a single merchant data

--- spec/facades/merchant_facade_spec.rb
+++ spec/facades/merchant_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe MerchantFacade do
   it 'can return a collection of merchant objects' do
-    merchants ||= MerchantFacade.all
+    merchants ||= MerchantFacade.index
 
     expect(merchants).to be_an Array
     expect(merchants.first).to be_a Merchant
--- spec/services/merchant_service_spec.rb
+++ spec/services/merchant_service_spec.rb
@@ -1,15 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe MerchantService do
-  it 'can return merchant data' do
-    response ||= MerchantService.all
+  it "can return multiple merchant's data" do
+    response ||= MerchantService.index
 
     expect(response[:data]).to be_an Array
     expect(response[:data].count).to eq(100)
 
     expect(response[:data].first[:id]).to eq('1')
+    expect(response[:data].first[:type]).to eq('merchant')
     expect(response[:data].first[:attributes][:name]).to eq('Schroeder-Jerde')
     expect(response[:data].last[:id]).to eq('42')
+    expect(response[:data].last[:type]).to eq('merchant')
     expect(response[:data].last[:attributes][:name]).to eq('Glover Inc')
+  end
+
+  it "can return one merchant's data" do
+    response ||= MerchantService.show(1)
+
+    expect(response[:data][:id]).to eq('1')
+    expect(response[:data][:type]).to eq('merchant')
+    expect(response[:data][:attributes][:name]).to eq('Schroeder-Jerde')
   end
 end
